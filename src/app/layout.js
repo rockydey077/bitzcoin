b8d2550d913e41ab34153b5a2ca1a3fdc2821258
rockydey077 <dev.rocky@jsencoder.com>
2024-08-22 09:28:38 +0600

    contact page design complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
-import { Inter, Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Shared/Navbar";
 
 // const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({
@@ -17,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={`${roboto.className} max-w-screen-2xl mx-auto`}>
-        {/* <Navbar /> */}
         <section>{children}</section>
       </body>
     </html>
